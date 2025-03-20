0b6cf4962d5713fa72f0639f8dba28777d275488
kishoww <kishorear.personal@gmail.com>
2025-03-20 22:06:20 +0530

    table updated

--- client/src/components/Table.jsx
+++ client/src/components/Table.jsx
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Table = (name = "Kishore") => {
+const Table = () => {
+
+    const [People, setPeople] = useState({
+        ashwin : false,
+        siva : false,
+        solasa : false,
+        sujith : false
+    });
+
+
     return (
         <div>
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -13,6 +22,9 @@ const Table = (name = "Kishore") => {
                             <th scope="col" class="px-6 py-3">
                                 Roll No
                             </th>
+                            <th>
+                                Status
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,6 +35,9 @@ const Table = (name = "Kishore") => {
                             <td class="px-6 py-4">
                                 CB.EN.U4ELC21009
                             </td>
+                            <td>
+                                { People.ashwin ? "Present" : "Absent"}
+                            </td>
                         </tr>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -30,6 +45,9 @@ const Table = (name = "Kishore") => {
                             </th>
                             <td class="px-6 py-4">
                                 CB.EN.U4ELC21020
+                            </td>
+                            <td>
+                                { People.siva ? "Present" : "Absent"}
                             </td>
                         </tr>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
@@ -39,6 +57,9 @@ const Table = (name = "Kishore") => {
                             <td class="px-6 py-4">
                                 CB.EN.U4ELC21057
                             </td>
+                            <td>
+                                { People.solasa ? "Present" : "Absent"}
+                            </td>
                         </tr>
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -46,6 +67,9 @@ const Table = (name = "Kishore") => {
                             </th>
                             <td class="px-6 py-4">
                                 CB.EN.U4ELC21069
+                            </td>
+                            <td>
+                                { People.sujith ? "Present" : "Absent"}
                             </td>
                         </tr>
                     </tbody>
