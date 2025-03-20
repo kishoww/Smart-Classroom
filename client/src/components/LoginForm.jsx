fef88a9ec09d2f10f60cb4564d5af7bdbe6fb2cd
kishoww <kishorear.personal@gmail.com>
2025-03-20 21:14:32 +0530

    Login form added

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -1,37 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
+    const navigate = useNavigate();
+
+    const [credentials, setCredentials] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleClick = (event) => {
+        event.preventDefault(); // Prevent page reload
+
+        if (["siva", "sujith", "ashwin", "solasa"].some(name => credentials.email.includes(name))) {
+            if (credentials.password === "Admin@123")
+                navigate("/dashboard/fans");
+            else {
+                alert("Wrong password bro");
+                credentials.password == "";
+            }
+        }
+    };
+
     return (
-        <section class="">
-            <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-
-                <div class="w-full bg-white border-1 border-gray-900 shadow-2xl rounded-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-black/90 flex justify-center">
-
-                    <div class="p-6 space-y-4 md:space-y-6 sm:p-10">
-
-                        <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <section className="">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white border-1 border-gray-900 shadow-2xl rounded-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-black/90 flex justify-center">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-10">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                             Sign in to your account
                         </h1>
-
-                        <form class="space-y-4 md:space-y-6" action="#">
+                        <form className="space-y-4 md:space-y-6">
                             <div>
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@amrita.com" required="" />
-                            </div>
-                            <div>
-                                <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="name@amrita.com"
+                                    required
+                                    value={credentials.email}
+                                    onChange={(event) => setCredentials((prev) => ({ ...prev, email: event.target.value }))}
+                                />
                             </div>
 
-                            <button type="submit" class="cursor-pointer w-full text-white bg-blue-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    placeholder="••••••••"
+                                    value={credentials.password}
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    required
+                                    onChange={(event) => setCredentials((prev) => ({ ...prev, password: event.target.value }))}
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="cursor-pointer w-full text-white bg-blue-600 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                onClick={handleClick}
+                            >
+                                Sign in
+                            </button>
                         </form>
-
                     </div>
-
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default LoginForm
+export default LoginForm;
--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
 import LoginForm from '../../components/LoginForm';
 
 const Login = () => {
-
-  const navigate = useNavigate();
-
-  const [Login, setLogin] = useState({
-    username: "",
-    password: ""
-  })
-
-  const handleClick = (event) => {
-    event.preventDefault();
-
-    if(Login.username === "" && Login.password === "")
-      navigate("/dashboard/fans");
-  }
 
   return (
     <div className='h-screen w-full bg-black'>
