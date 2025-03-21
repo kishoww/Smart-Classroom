55429b5204e82143ea26fcd16caf5910b326d018
kishoww <kishorear.personal@gmail.com>
2025-03-21 12:37:11 +0530

    final commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -62,7 +62,7 @@ const Sidebar = () => {
               },
               {
                 path: "/dashboard/security",
-                label: "Security Camera",
+                label: "Camera",
                 icon: (
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-cctv">
                     <path d="M16.75 12h3.632a1 1 0 0 1 .894 1.447l-2.034 4.069a1 1 0 0 1-1.708.134l-2.124-2.97" />
